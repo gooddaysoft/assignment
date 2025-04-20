36bf44ac0ffbf25cdb9b3fb74d70b2eefc0ec80d
jay0425 <heedev94@gmail.com>
2025-04-21 03:07:19 +0900

    feat: dashboard ui 구현

--- my-web-app/app/dashboard/DashboardClient.tsx
+++ my-web-app/app/dashboard/DashboardClient.tsx
@@ -2,8 +2,27 @@
 
 export default function DashboardClient() {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <p>대쉬보드 입니다.</p>
-    </div>
+    <main className="flex justify-center items-center min-h-screen bg-gray-50">
+      <section className="p-8 space-y-6 bg-pink-50 rounded-lg shadow-md">
+        <header className="flex justify-start items-end gap-3">
+          <h1 className="text-2xl font-bold">재희 님</h1>
+          <span className="text-lg font-semibold">team</span>
+        </header>
+        <dl className="grid grid-cols-2 gap-4">
+          <div className="p-4 bg-pink-200 rounded-xl shadow">
+            <dt className="font-medium">이번 달 청구 예정 금액</dt>
+            <dd>
+              <strong>100원</strong>
+            </dd>
+          </div>
+          <div className="p-4 bg-pink-200 rounded-xl shadow">
+            <dt className="font-medium">총 사용량</dt>
+            <dd>
+              <strong>100건</strong>
+            </dd>
+          </div>
+        </dl>
+      </section>
+    </main>
   );
 }
