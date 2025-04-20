0474102309497b50246c3a6d45193382e25913f7
jay0425 <heedev94@gmail.com>
2025-04-20 15:01:44 +0900

    feat: 프로젝트 초기 환경설정 완료 및 기본 라우팅 구조 구현

---
+++ my-web-app/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <main className="flex min-h-screen items-center justify-center bg-blue-100">
+      <h1 className="text-3xl font-bold text-blue-600">Tailwind 설정 완료!</h1>
+    </main>
+    </>
+  );
+}
