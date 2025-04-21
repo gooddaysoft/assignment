7ddb9e8b8c8737a278c330330afa11d08427b9f8
jay0425 <heedev94@gmail.com>
2025-04-21 18:00:45 +0900

    feat: Subscription Detail 페이지 데이터 렌더링 구현
    - 없는 url 접근시 404 처리

--- my-web-app/app/plans/[planId]/page.tsx
+++ my-web-app/app/plans/[planId]/page.tsx
@@ -1,15 +1,32 @@
 import Link from 'next/link';
+import { Plan } from '../../api/types';
+import { notFound } from 'next/navigation';
 
-export default async function PlanDetailPage() {
+export default async function PlanDetailPage({ params }: { params: { planId: string } }) {
+  console.log(params.planId);
+
+  const res = await fetch('http://localhost:3000/api/plans', {
+    cache: 'no-store',
+  });
+  const plans: Plan[] = await res.json();
+
+  console.log(plans);
+
+  const plan = plans.find((p) => p.id === params.planId);
+
+  console.log(plan);
+
+  if (!plan) return notFound();
+
   return (
     <main className="flex justify-center items-center min-h-screen bg-gray-50">
       <section className="w-[25rem] h-[23rem] p-10 bg-white rounded shadow-lg text-center">
-        <h1 className="text-3xl font-bold mb-3">Basic</h1>
-        <p className="text-xl font-semibold mb-12">0원</p>
+        <h1 className="text-3xl font-bold mb-3">{plan.name}</h1>
+        <p className="text-xl font-semibold mb-12">{plan.price}원</p>
         <ul className="flex justify-evenly mb-16">
-          <li>10 users</li>
+          <li>{plan.features[0]}</li>
           <li> / </li>
-          <li>5 GB storage</li>
+          <li>{plan.features[1]}</li>
         </ul>
         <Link href="/dashboard" className="bg-pink-100 hover:bg-pink-200 px-6 py-2.5 rounded font-semibold">
           구독 확정
